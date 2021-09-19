b4b47f930b07b84871370ee22f6838c9bbd091bd
ankitAalekh <ankitbokaro1996@gmail.com>
2021-09-20 02:50:51 +0530

    Working with arrays and objects in typescript

--- basic.ts
+++ basic.ts
@@ -8,7 +8,23 @@ let age: number;
 age = 24;
 
 let username: string;
-username='Ankit';
+username = "Ankit";
 
-let  isWorking : boolean;
+let isWorking: boolean;
 isWorking: true;
+
+//More complex types
+
+let hobbies: string[];
+hobbies = ["sketching", "singing"];
+
+let person: { name: string; age: number };
+person = {
+  name: "Ankit",
+  age: 25,
+};
+
+let people: {
+    name: string,
+    age: number,
+}[];
