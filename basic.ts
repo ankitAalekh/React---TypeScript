03d03c5feb78f997fa577ec2fea9415fe73923a2
ankitAalekh <ankitbokaro1996@gmail.com>
2021-09-20 02:38:35 +0530

    TypeScript basics

---
+++ basic.ts
@@ -0,0 +1,14 @@
+//Primitives : number, string, boolean
+//More complex types: arrays, objects
+//Function types, parameters
+//Primitives
+
+let age: number;
+
+age = 24;
+
+let username: string;
+username='Ankit';
+
+let  isWorking : boolean;
+isWorking: true;
